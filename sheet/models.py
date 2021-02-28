983e4086d710e6960adb445fd2b972f4b86fac3a
Ichmed <mijomeyer@t-online.de>
2021-02-28 21:21:00 +0100

    fixed Cascade bug

--- sheet/models.py
+++ sheet/models.py
@@ -4,7 +4,7 @@ from django.db.models import *
 
 class Character(Model):
 	name = CharField(max_length=255)
-	race = ForeignKey('rulebook.Race', on_delete='Cascade')
+	race = ForeignKey('rulebook.Race', on_delete=CASCADE)
 	
 	CON = IntegerField(default=10)
 	STR_mod = IntegerField(default=0)
@@ -50,31 +50,31 @@ class Character(Model):
 
 
 class AbilityInstance(Model):
-	base = ForeignKey('rulebook.Ability', blank=True, null=True, on_delete='Cascade')
+	base = ForeignKey('rulebook.Ability', blank=True, null=True, on_delete=CASCADE)
 	name = CharField(max_length=255)
 	level = IntegerField()
-	owner = ForeignKey(Character, related_name='abilities', on_delete='Cascade')
+	owner = ForeignKey(Character, related_name='abilities', on_delete=CASCADE)
 	
 
 	def __str__(self):
 		return self.name + " " + str(self.pk)
 
 class SkillInstance(Model):
-	base = ForeignKey('rulebook.Skill', blank=True, null=True, on_delete='Cascade')
+	base = ForeignKey('rulebook.Skill', blank=True, null=True, on_delete=CASCADE)
 	name = CharField(max_length=255)
 	level = IntegerField(default=1)
 	granted = IntegerField(default=0)
-	owner = ForeignKey(Character, related_name='skills', on_delete='Cascade')
+	owner = ForeignKey(Character, related_name='skills', on_delete=CASCADE)
 	
 	def __str__(self):
 		return self.name + " " + str(self.pk)
 
 class InventoryItem(Model):
-	base = ForeignKey('rulebook.Item', blank=True, null=True, on_delete='Cascade')
+	base = ForeignKey('rulebook.Item', blank=True, null=True, on_delete=CASCADE)
 	name = CharField(max_length=255)
 	text = CharField(max_length=2000, blank=True)
-	owner = ForeignKey(Character, blank=True, null=True, on_delete='Cascade', related_name='inventory')
-	container = ForeignKey('self', blank=True, null=True, on_delete='Cascade', related_name='content')
+	owner = ForeignKey(Character, blank=True, null=True, on_delete=CASCADE, related_name='inventory')
+	container = ForeignKey('self', blank=True, null=True, on_delete=CASCADE, related_name='content')
 	weight = IntegerField(default=0)
 	amount = IntegerField(default=1)
 	
@@ -82,25 +82,25 @@ class InventoryItem(Model):
 		return self.name + " " + str(self.pk)
 
 class Resistance(Model):
-	owner = ForeignKey(Character, related_name='resistances', on_delete='Cascade')
-	type = ForeignKey('rulebook.DamageType', on_delete='Cascade')
+	owner = ForeignKey(Character, related_name='resistances', on_delete=CASCADE)
+	type = ForeignKey('rulebook.DamageType', on_delete=CASCADE)
 	amount = IntegerField(default=0)
 
 	def __str__(self):
 		return f"{self.owner} {self.type} {self.amount}"
 
 class Wound(Model):
-	type = ForeignKey('rulebook.DamageType', on_delete='Cascade')
+	type = ForeignKey('rulebook.DamageType', on_delete=CASCADE)
 	size = IntegerField()
 	healed = IntegerField()
 	state = CharField(default="F", max_length=1, choices=[('F', 'Fresh'), ('T', 'Treated'), ('O', 'Old')])
-	owner = ForeignKey(Character, related_name='wounds', on_delete='Cascade')
+	owner = ForeignKey(Character, related_name='wounds', on_delete=CASCADE)
 	
 	def __str__(self):
 		return f"{self.owner} {self.healed}/{self.size}({self.type})"
 
 class Note(Model):
-	owner = ForeignKey(Character, related_name='notes', on_delete='Cascade')
+	owner = ForeignKey(Character, related_name='notes', on_delete=CASCADE)
 	text = CharField(max_length=2000)
 	expiration = IntegerField()
 	
