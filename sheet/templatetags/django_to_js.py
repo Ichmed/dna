f62a0da4535051617a2eddec4784c181b622588c
Ichmed <mijomeyer@t-online.de>
2021-03-06 00:36:46 +0100

    continued work on django-to-js, including for loops, ifs and plain text

--- sheet/templatetags/django_to_js.py
+++ sheet/templatetags/django_to_js.py
@@ -2,11 +2,11 @@ from django.template.defaulttags import register
 from django.utils.safestring import mark_safe
 
 @register.filter
-def to_js_template(path, source="data"):
-	text = open(path).read()
-	return mark_safe("var _R = \"\";" + parse(text, source=source))
+def to_js_template(path, target="r", source="data"):
+	text = open("sheet/templates/" + path).read()
+	return mark_safe(f"var _{target} = \"\";" + parse(text, target=target, source=source)[0])
 
-def parse(block, header="", source="data"):
+def parse(block, header="", target="r", source="data"):
 	result = ""
 
 	beginning = 0
@@ -16,7 +16,7 @@ def parse(block, header="", source="data"):
 		type, preamble = parse_header(header)
 	else:
 		type = ""
-		preamble = "{"
+		preamble = ""
 
 	while True:
 		try:		
@@ -24,7 +24,7 @@ def parse(block, header="", source="data"):
 			stop = block.index("%}", start) + 2
 
 			if start > beginning:
-				result += format_plain(block[beginning:start])
+				result += format_plain(block[beginning:start], target)
 
 			beginning = stop
 
@@ -42,28 +42,57 @@ def parse(block, header="", source="data"):
 			beginning += offset
 		except ValueError:
 			if end > beginning:
-				result += format_plain(block[beginning:end])
+				result += format_plain(block[beginning:end], target)
 			break
 	
 	# print("Result:", result)
 	# print()
 
-	return preamble + result + "}", end
+	return preamble + result + ("}\n" if "{" in preamble else "\n"), end
 
-def format_plain(s):
-	return "_R += \"" + s.replace("\n", "\\n") + "\";"
+escapes = {
+	"\n":  "\\n",
+	"\"":  "\\\"",
+	"'": "\\'",
+}
+
+subs = {
+	"{{": "\" +",
+	"}}": "+ \"",
+	"|cat:": "+",
+}
+
+def escape(x):
+	for a, b in escapes.items():
+		x = x.replace(a, b)
+	return x
+
+def substitute(x):
+	for a, b in subs.items():
+		x = x.replace(a, b)
+	return x
+
+def format_plain(s, target="r"):
+	return f"_{target} += \"" + substitute(escape(s)) + "\";\n"
 
 def parse_header(header):
 	header = header.strip()
 
 	type, *rest = header.split()
-	preamble = "{"
+	preamble = ""
 
 	if type == "for":
 		running, _, source= rest
 		preamble = f"for({running}_ in {source}){{ var {running} = {source}[{running}_]; "
 	elif type == "if":
-		preamble = "(" + " ".join(rest) + ") {"
+		preamble = "if (" + " ".join(rest) + ") {"
+	elif type == "with":
+		f, _, t = rest
+		print(f"{{ var {t} = {substitute(f)};\n")
+		preamble = f"{{ var {t} = {substitute(f)};"
+	# TODO: self closing tags
+	# elif type == "static":
+	# 	preamble = "\" + " + " ".join(rest) + " + \""
 
 	return type, preamble
 	
