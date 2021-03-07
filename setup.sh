05fedaf453b736ba58b9f3b6b70c57952dbd1887
Ichmed <mijomeyer@t-online.de>
2021-03-07 22:05:01 +0100

    Added migration to setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ python -m pip install --upgrade pip
 echo "" > dna/secret
 pip install -r requirements.txt
 python setup.py build_ext --inplace
+python manage.py migrate
 python manage.py collectstatic --no-input
